16f0390142228443500253ccc1834eaf366b1a34
aarushi2146 <106542499+aarushi2146@users.noreply.github.com>
2025-03-06 11:43:50 +0530

    Delete EmployeePayrollApp-UC1 directory

--- EmployeePayrollApp-UC1/EmployeePayrollApp/src/main/java/com/bridgelabz/EmployeePayrollApp/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz.EmployeePayrollApp.repository;
-
-import com.bridgelabz.EmployeePayrollApp.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
